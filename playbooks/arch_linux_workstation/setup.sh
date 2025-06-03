48a94e5556cd9668b484ebfa861905f3daaa65a6
Olafur Gudmundsson <1510904+olafurg@users.noreply.github.com>
2025-06-03 22:56:57 +0000

    Typo

--- playbooks/arch_linux_workstation/setup.sh
+++ playbooks/arch_linux_workstation/setup.sh
@@ -7,4 +7,4 @@ sudo pacman -Syu
 sudo pacman -S --needed git curl ansible
 
 # Run the playbook with locale set
-LANG=C.UTF-8 LC_ALL=C.UTF-8 ansible-playbook playbooks/arch_linux/playbook.yml -K
+LANG=C.UTF-8 LC_ALL=C.UTF-8 ansible-playbook playbooks/arch_linux_workstation/playbook.yml -K
