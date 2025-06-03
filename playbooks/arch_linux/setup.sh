c6e649f90fabde49caf8c6c926551a54d06f4346
Olafur Gudmundsson <1510904+olafurg@users.noreply.github.com>
2025-06-03 22:14:28 +0000

    Add arch (#7
    Adding some Arch stuff in the same spirit. More needed later on.

---
+++ playbooks/arch_linux/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Upgrade the system
+sudo pacman -Syu
+
+# Install prerequisites
+sudo pacman -S --needed git curl ansible
+
+# Run the playbook with locale set
+LANG=C.UTF-8 LC_ALL=C.UTF-8 ansible-playbook playbooks/arch_linux/playbook.yml -K
---
+++ roles/oh_my_zsh/files/custom/aliases_arch_linux.zsh
@@ -0,0 +1,7 @@
+# Aliases for zsh
+
+alias lla='ls -lah'
+alias rm='rm -i'
+alias mv='mv -i'
+alias digs='dig +short'
+alias update='sudo pacman -Syu'
