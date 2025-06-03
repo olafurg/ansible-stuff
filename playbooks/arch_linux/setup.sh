08d5491719d427360a795c795fe1e04012f4758e
Olafur Gudmundsson <1510904+olafurg@users.noreply.github.com>
2025-06-03 21:31:45 +0000

    Temp fix

--- playbooks/arch_linux/setup.sh
+++ playbooks/arch_linux/setup.sh
@@ -4,7 +4,16 @@
 sudo pacman -Syu
 
 # Install prerequisites
-sudo pacman -S git curl ansible
+sudo pacman -S --needed git curl ansible
+
+# Setup locale if none is set
+if [ ! -f /etc/locale.conf ] || [ -z "$(grep '^LANG=' /etc/locale.conf)" ]; then
+    echo "No locale configured, setting up default locale..."
+    echo "en_US.UTF-8 UTF-8" | sudo tee /etc/locale.gen
+    sudo locale-gen
+    echo "LANG=en_US.UTF-8" | sudo tee /etc/locale.conf
+    echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/locale.conf
+fi
 
 # Run the playbook
 ansible-playbook playbooks/arch_linux/playbook.yml -K
